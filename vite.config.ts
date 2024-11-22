4ef1d911581519f94fc09ac8cc753271303bde01
Afiq Nurhariz <afiqezio@gmail.com>
2024-11-22 18:44:38 +0800

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,11 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+  // server: {
+  //   host: "::",
+  //   port: 8080,
+  // },
+  plugins: [react()],
   base: '/afiq-nurhariz/',
   resolve: {
     alias: {

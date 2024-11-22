2aaf36c4a15ac410ea4ea95b7d880a6565828cc9
Afiq Nurhariz <afiqezio@gmail.com>
2024-11-22 21:38:05 +0800

    Sync Production

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={'/afiq-nurhariz/'}>
         <Routes>
           <Route path="/" element={<Index />} />
         </Routes>
